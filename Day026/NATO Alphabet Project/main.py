74f611ae776c5ca90db9321c7968d04424da6064
Hasib Kamal <37206019+Hasib104@users.noreply.github.com>
2021-01-25 23:31:08 +0600

    Update main.py

--- Day026/NATO Alphabet Project/main.py
+++ Day026/NATO Alphabet Project/main.py
@@ -8,5 +8,8 @@ nato_dict = {row.letter:row.code for (index, row) in nato.iterrows()}
 #print(nato_dict)
 
 word = input("Enter a word: ").upper()
-output_list = [nato_dict[item] for item in word]
+output_list = [nato_dict[item] for item in word] #using list comprehension
+# output_list = [] #using conventional loop
+# for item in word:
+#     output_list.append(nato_dict[item])
 print(output_list)
