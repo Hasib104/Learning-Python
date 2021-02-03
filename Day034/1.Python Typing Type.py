49fd056b3474d9ca9af0f4d9228c07bd723696a5
Hasib Kamal <37206019+Hasib104@users.noreply.github.com>
2021-02-03 17:42:09 +0600

    Add files via upload

---
+++ Day034/1.Python Typing Type.py
@@ -0,0 +1,22 @@
+﻿
+"""#definig the type in the start"""
+age:int
+name:str
+height:float
+is_human:bool
+
+#so that when we assign its value it will expect the expected type
+
+def police_check(age: int) -> bool: #specifying the return type to bool using ->
+    if age > 18:
+        can_drive = True
+    else:
+        can_drive = False
+    return can_drive
+
+print(police_check(12))
+
+if police_check(19):
+    print("You may pass.")
+else:
+    print("Pay a fine.")
---
+++ Day034/Quizzler/ui.py
@@ -0,0 +1,61 @@
+
+from tkinter import *
+from quiz_brain import QuizBrain
+
+THEME_COLOR = "#375362"
+
+class QuizInterface:
+
+    def __init__(self, quiz_brain: QuizBrain):
+
+        self.quiz = quiz_brain
+
+        self.window = Tk()
+        self.window.title("Quizzleer")
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+
+        self.score = Label(text= "Score: 0", bg=THEME_COLOR, fg="white")
+        self.score.grid(column=1, row=0)
+
+        self.canvas = Canvas(width= 300, height= 250)
+        self.question_text = self.canvas.create_text(150, 125, width=250, text="Questions", font=("Arial", 20, "italic"), fill="black")
+        self.canvas.grid(column=0, row=1, columnspan= 2, pady=20)
+
+        true_button_img = PhotoImage(file="images/true.png")
+        self.true_button = Button(image=true_button_img, highlightthickness=0, command=self.true_pressed)
+        self.true_button.grid(column=0, row=2)
+
+        false_button_img = PhotoImage(file="images/false.png")
+        self.false_button = Button(image=false_button_img, highlightthickness=0, command=self.false_pressed)
+        self.false_button.grid(column=1, row=2)
+
+        self.get_next_question()
+
+        self.window.mainloop()
+
+    def get_next_question(self):
+        self.canvas.config(bg="white")
+
+        if self.quiz.still_has_questions():
+            self.score.config(text=f"Score: {self.quiz.score}")
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question_text, text=q_text)
+        else:
+            self.canvas.itemconfig(self.question_text, text="You've reached the end.")
+            self.true_button.config(state="disabled")
+            self.false_button.config(state="disabled")
+
+    def true_pressed(self):
+        is_right = self.quiz.check_answer("True")
+        self.give_feedback(is_right)
+
+    def false_pressed(self):
+        is_right = self.quiz.check_answer("False")
+        self.give_feedback(is_right)
+
+    def give_feedback(self, is_right):
+        if is_right:
+            self.canvas.config(bg="green")
+        else:
+            self.canvas.config(bg="red")
+        self.window.after(1000, self.get_next_question)
