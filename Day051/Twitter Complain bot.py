d9574eb1eb6f90a5dc80d5d096a64dd40c472b53
Hasib Kamal <37206019+Hasib104@users.noreply.github.com>
2021-02-16 15:30:07 +0600

    Add files via upload

---
+++ Day051/Twitter Complain bot.py
@@ -0,0 +1,74 @@
+﻿
+from selenium import webdriver
+import time
+from selenium.webdriver.common.keys import Keys
+
+chrome_driver_path = "C:\Development\chromedriver.exe"
+
+
+promised_down = 100
+promised_up = 100
+
+twitter_username = "araf_hasib"
+PASSWORD = "Pp123456"
+
+class InternetSpeedTwitterBot:
+    def __init__(self):
+        self.driver = webdriver.Chrome(executable_path=chrome_driver_path)
+        self.down = 0
+        self.up = 0
+
+    def get_speed(self):
+        self.driver.get("https://www.speedtest.net/")
+
+        go = self.driver.find_element_by_xpath('//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[1]/a')
+        go.click()
+
+        time.sleep(40)
+
+        self.download_speed = self.driver.find_element_by_xpath('//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[2]/div/div[2]/span').text
+        print(self.download_speed)
+        self.upload_speed = self.driver.find_element_by_xpath('//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[3]/div/div[2]/span').text
+        print(self.upload_speed)
+
+    def post_on_twitter(self):
+        self.driver.get(url="https://twitter.com/?lang=en")
+        time.sleep(2)
+
+        log_in = self.driver.find_element_by_xpath(
+            '//*[@id="react-root"]/div/div/div/main/div/div/div/div[1]/div/div[3]/a[2]/div')
+        log_in.click()
+
+        username = self.driver.find_element_by_xpath(
+            '//*[@id="react-root"]/div/div/div[2]/main/div/div/div[2]/form/div/div[1]/label/div/div[2]/div/input')
+        username.send_keys(twitter_username)
+
+        password = self.driver.find_element_by_xpath(
+            '//*[@id="react-root"]/div/div/div[2]/main/div/div/div[2]/form/div/div[2]/label/div/div[2]/div/input')
+        password.send_keys(PASSWORD)
+
+        log_in_1 = self.driver.find_element_by_xpath(
+            '//*[@id="react-root"]/div/div/div[2]/main/div/div/div[2]/form/div/div[3]/div/div')
+        log_in_1.click()
+
+        time.sleep(5)
+        tweet_compose = self.driver.find_element_by_xpath(
+            '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[1]/div/div/div/div/div/div/div/div/div/div[1]/div/div/div/div[2]/div/div/div/div')
+        tweet = f"Hey @amberiteu, why is my internet speed {self.download_speed}down/{self.upload_speed}up when I pay for {promised_down}down/{promised_up}up?"
+        tweet_compose.send_keys(tweet)
+
+        time.sleep(3)
+        tweet_button = self.driver.find_element_by_xpath(
+            '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[4]/div/div/div[2]/div[3]')
+        tweet_button.click()
+
+        time.sleep(2)
+
+bot = InternetSpeedTwitterBot()
+bot.get_speed()
+bot.post_on_twitter()
+
+
+
+
+
