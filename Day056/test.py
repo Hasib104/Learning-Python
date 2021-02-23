d00639a44ef02c5e8abf73069ab882608e089757
Hasib Kamal <37206019+Hasib104@users.noreply.github.com>
2021-02-23 19:39:31 +0600

    Add files via upload

---
+++ Day056/test.py
@@ -0,0 +1,13 @@
+﻿
+def remove_all_before(items, border):
+    for i in range(len(items)):
+
+        if items[i] == border:
+            print(items[i:])
+
+remove_all_before([1, 2, 3, 4, 5], 3)
+remove_all_before([1, 1, 2, 2, 3, 3], 2)
+remove_all_before([1, 1, 2, 4, 2, 3, 4], 2)
+remove_all_before([1, 1, 5, 6, 7], 2)
+remove_all_before([], 0)
+remove_all_before([7, 7, 7, 7, 7, 7, 7, 7, 7], 7)
