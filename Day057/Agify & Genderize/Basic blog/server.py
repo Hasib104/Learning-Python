2f2a7b019e4d037c64dc41ff31d6b40bee0fe92b
Hasib Kamal <37206019+Hasib104@users.noreply.github.com>
2021-02-23 20:07:50 +0600

    Add files via upload

---
+++ Day057/Agify & Genderize/Basic blog/server.py
@@ -0,0 +1,20 @@
+﻿
+from flask import Flask, render_template
+import requests
+
+app = Flask(__name__)
+
+@app.route("/")
+def function():
+    return "Hello World."
+
+@app.route("/blog")
+def blog():
+    url = "https://api.npoint.io/5abcca6f4e39b4955965"
+    response = requests.get(url=url)
+    posts = response.json()
+    return render_template("index.html", posts=posts)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
